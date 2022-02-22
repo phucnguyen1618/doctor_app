39baaf06391f16fd5a897c524adbf52299474385
phucnguyen1618 <phucnguyen.mobiledeveloper@gmail.com>
2022-02-22 11:44:18 +0700

    update project: add image.

--- lib/app/core/assets/constants/image_constants.dart
+++ lib/app/core/assets/constants/image_constants.dart
@@ -4,4 +4,7 @@ class ImageConstants {
   static const String splash = AssetConstants.imageConstants + 'splash.png';
   static const String login = AssetConstants.imageConstants + 'login.png';
   static const String finAuth = AssetConstants.imageConstants + 'fin_auth.png';
+  static const String googleImage = AssetConstants.imageConstants + 'google.png';
+  static const String facebookImage = AssetConstants.imageConstants + 'facebook.png';
+  static const String twitterImage = AssetConstants.imageConstants + 'twitter.png';
 }
--- lib/app/modules/login/login_page.dart
+++ lib/app/modules/login/login_page.dart
@@ -1,7 +1,6 @@
 import 'package:doctor_app/app/core/assets/constants/icon_constants.dart';
 import 'package:doctor_app/app/core/assets/constants/image_constants.dart';
 import 'package:doctor_app/app/core/widgets/custom_button.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
